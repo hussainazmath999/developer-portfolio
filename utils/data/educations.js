eb3dd8d7924944d654adf428a21ef3556c439a8a
Azmath hussain mohammed <azmathhussain001@gmail.com>
2025-01-16 01:19:06 -0500

    Updated education, experience, personal data, and skills in utils/data

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 export const educations = [
   {
     id: 1,
@@ -13,20 +12,4 @@ export const educations = [
     institution: "Velagapudi Ramakrishna Siddhartha Engineering College",
   },
   
-=======
-export const educations = [
-  {
-    id: 1,
-    title: "Master of Science in Computer Science.",
-    duration: "Jan 2023 - May 2024",
-    institution: "University of North Carolina at Charlotte",
-  },
-  {
-    id: 2,
-    title: "Bachelor of Technology in Computer Science",
-    duration: "2017 - 2021",
-    institution: "Velagapudi Ramakrishna Siddhartha Engineering College",
-  },
-  
->>>>>>> cb6167bece9f159ffdc72cf93ede30f5cfeaf972
 ]
--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -3,11 +3,7 @@ export const personalData = {
   profile: '/profile.png',
   designation: "Software Developer",
   description: "My name is Azmath hussain Mohammed. I am a professional and enthusiastic programmer in my daily life. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. I love almost all the stacks of web application development and love to make the web more open to the world. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
-<<<<<<< HEAD
   email: 'azmathhussain046@gmail.com',
-=======
-  email: 'azmathhussain001@gmail.com',
->>>>>>> cb6167bece9f159ffdc72cf93ede30f5cfeaf972
   phone: '+1980-895-0392',
   address: 'North carolina charlotte ',
   github: 'https://github.com/said7388',
@@ -17,9 +13,5 @@ export const personalData = {
   stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
   leetcode: "https://leetcode.com/said3812/",
   devUsername: "said7388",
-<<<<<<< HEAD
   resume: "https://drive.google.com/file/d/18x8StqLj2IzqzQ7x5O-LP24V935ug2FH/view?usp=sharing"
-=======
-  resume: "https://drive.google.com/file/d/1B7Hq4vQu-egiwRcP9cnGbqQ4pVMU0nS1/view"
->>>>>>> cb6167bece9f159ffdc72cf93ede30f5cfeaf972
 }
